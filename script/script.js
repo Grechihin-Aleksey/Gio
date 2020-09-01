1ed74c94e7def8ac568afebab44251d0e1797a8f
Aleksey Grechihin <9108921170@dz-led.ru>
2020-09-01 19:31:12 +0300

    Исправил

--- script/script.js
+++ script/script.js
@@ -1,90 +1,54 @@
-window.addEventListener('DOMContentLoaded', () => {
-    'use strict';
-    // Timer
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+  // Timer
 
-    const countTimer = (dedline) => {
+  const countTimer = (dedline) => {
+    const timerHours = document.querySelector("#timer-hours"),
+      timerMinutes = document.querySelector("#timer-minutes"),
+      timerSecunds = document.querySelector("#timer-seconds");
+    let idInterval = 0;
 
-        const timerHours = document.querySelector('#timer-hours'),
+    const getTimeRemaining = () => {
+      const dateStop = new Date(dedline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000;
+      let seconds = 0,
+        minutes = 0,
+        hours = 0;
 
-            timerMinutes = document.querySelector('#timer-minutes'),
+      if (timeRemaining > 0) {
+        seconds = Math.floor(timeRemaining % 60);
+        minutes = Math.floor((timeRemaining / 60) % 60);
+        hours = Math.floor(timeRemaining / 60 / 60);
+      }
 
-            timerSecunds = document.querySelector('#timer-seconds');
-
-        let idInterval = 0;
-
-
-
-        const getTimeRemaining = () => {
-
-            const dateStop = new Date(dedline).getTime(),
-
-                dateNow = new Date().getTime(),
-
-                timeRemaining = (dateStop - dateNow) / 1000;
-
-            let seconds = 0,
-
-                minutes = 0,
-
-                hours = 0;
-
-            if (timeRemaining > 0) {
-
-                seconds = Math.floor(timeRemaining % 60);
-
-                minutes = Math.floor((timeRemaining / 60) % 60);
-
-                hours = Math.floor(timeRemaining / 60 / 60);
-
-            }
-
-            return {
-
-                timeRemaining,
-
-                hours,
-
-                minutes,
-
-                seconds
-
-            };
-
-        };
-
-
-        const addZero = elem => {
-
-            if (String(elem).length === 1) {
-                return '0' + elem;
-            } else {
-                return String(elem);
-            }
-
-        };
-
-
-
-        const updateClock = () => {
-
-            let timer = getTimeRemaining();
-
-            timerHours.textContent = addZero(timer.hours);
-
-            timerMinutes.textContent = addZero(timer.minutes);
-
-            timerSecunds.textContent = addZero(timer.seconds);
-
-
-            if (timer.timeRemaining < 0) {
-
-                clearInterval(idInterval);
-
-            }
-
-        };
-        idInterval = setInterval(updateClock, 1000);
+      return {
+        timeRemaining,
+        hours,
+        minutes,
+        seconds,
+      };
     };
-    countTimer('02 September 2020');
 
+    const addZero = (elem) => {
+      if (String(elem).length === 1) {
+        return "0" + elem;
+      } else {
+        return String(elem);
+      }
+    };
+
+    const updateClock = () => {
+      let timer = getTimeRemaining();
+      timerHours.textContent = addZero(timer.hours);
+      timerMinutes.textContent = addZero(timer.minutes);
+      timerSecunds.textContent = addZero(timer.seconds);
+
+      if (timer.timeRemaining < 0) {
+        clearInterval(idInterval);
+      }
+    };
+    idInterval = setInterval(updateClock, 1000);
+  };
+  countTimer("02 September 2020");
 });
